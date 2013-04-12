05c7a06a0c90efa14d409933637f788a2af71298
Ajay Sharma <dudleydragon@hotmail.com>
2013-04-11 19:45:25 -0700

    Attribute clean up

--- app/models/moment.rb
+++ app/models/moment.rb
@@ -1,4 +1,24 @@
 class Moment < ActiveRecord::Base
-  belongs_to :author
-  attr_accessible :description, :name, :token, :trash
+  
+  # keep the default scope first (if any)
+
+  # constants come up next
+
+  # afterwards we put attr related macros
+  attr_accessible :description, :name
+
+  # followed by association macros  
+  belongs_to :author, :foreign_key => "author_id"
+  
+  # and validation macros
+  validates :name, :presence => true
+
+  # next we have callbacks
+  after_initialize :default_values, if: 'new_record?' # set defaults on new records
+  
+  # set the default values for the Moment
+  def default_values
+    self.trash ||= false
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
   rolify
+  
   attr_accessible :role_ids, :as => :admin
   attr_accessible :provider, :uid, :name, :email
+  
   validates_presence_of :name
+
+  has_many :moments, :foreign_key => "author_id"
 
   def self.create_with_omniauth(auth)
     create! do |user|
