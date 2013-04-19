fda019682598449f0aac093da7e171c28f91d461
Ajay Sharma <dudleydragon@hotmail.com>
2013-04-19 13:36:48 -0700

    Show hearts for each moment, if any

--- app/models/moment.rb
+++ app/models/moment.rb
@@ -9,6 +9,8 @@ class Moment < ActiveRecord::Base
 
   # followed by association macros  
   belongs_to :author, :foreign_key => "author_id", :class_name => "User"
+  has_many :responses
+  has_many :hearted_responses, :class_name => "Response", :conditions => "hearts_count > 0"
   
   # and validation macros
   validates :name, :presence => true
